c0ae6d3b395844cd20c3fa949ee521d3a67a9868
b1acksun <tkachenko.igor88@gmail.com>
2017-12-05 10:20:51 +0200

    fix typo

--- test/all_types.js
+++ test/all_types.js
@@ -63,7 +63,7 @@ let allTypes = {
     time_point_sec3: '2017-02-16T20:27:12',
 }
 
-describe("golos.auth: all types", ()=> {
+describe("golos.types", ()=> {
 
     let { toObject, fromObject, toBuffer, fromBuffer } = AllTypes
 
