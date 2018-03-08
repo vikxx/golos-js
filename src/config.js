a0f1382eb5602bc8542b6ecc8b8314a40242b50c
b1acksun <tkachenko.igor88@gmail.com>
2018-03-08 18:23:46 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -1,10 +1,24 @@
+import each from 'lodash/each';
 const defaultConfig = require('../config.json');
 
-module.exports = (function () {
-  const config = defaultConfig;
-  const get = (key) => config[key];
-  const set = (key, value) => {
-    config[key] = value;
-  };
-  return { get, set };
-})();
+class Config {
+    constructor(c) {
+        each(c, (value, key) => {
+            this[key] = value;
+        });
+    }
+
+    get(k) {
+        return this[k];
+    }
+
+    set(k, v) {
+        this[k] = v;
+    }
+}
+
+module.exports = new Config(defaultConfig);
+if(typeof module.exports.Config !== 'undefined') {
+    throw new Error("default config.json file may not contain a property 'Config'");
+}
+module.exports.Config = Config;
